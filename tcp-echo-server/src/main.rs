18d7df73d2b8e5d0a6663ababbaa450cefbf264d
Hiroaki Nakamura <hnakamur@gmail.com>
2020-08-27 00:20:26 +0900

    Check fd of tokio TcpListener
    The output was:
    fd=8, ret=1, fd_cloexec=1, has_f_get_fd=1, new_value=0

--- tcp-echo-server/src/main.rs
+++ tcp-echo-server/src/main.rs
@@ -1,4 +1,5 @@
 use listenfd::ListenFd;
+use std::os::unix::io::AsRawFd;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
 
@@ -10,6 +11,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     } else {
         TcpListener::bind("127.0.0.1:8080").await?
     };
+    let fd = listener.as_raw_fd();
+    let flags = unsafe { libc::fcntl(fd, libc::F_GETFD) };
+    if flags == -1 {
+        panic!("fcntl F_GETFD failed");
+    }
+    println!("fd={}, ret={}, fd_cloexec={}, has_f_get_fd={}, new_value={}", fd, flags, libc::FD_CLOEXEC, flags & libc::FD_CLOEXEC, flags & !libc::FD_CLOEXEC);
 
     loop {
         let (mut socket, _) = listener.accept().await?;
