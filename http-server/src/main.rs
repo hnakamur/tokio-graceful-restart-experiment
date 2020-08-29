204d6dc6aed01ca26bf348cbe82dd6b67e683817
Hiroaki Nakamura <hnakamur@gmail.com>
2020-08-29 18:05:04 +0900

    Use listenfd to receive listener from parent in http server

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{web, App, Responder, HttpServer};
+use listenfd::ListenFd;
+use std::net::TcpListener;
 
 async fn index(info: web::Path<(String, u32)>) -> impl Responder {
     format!("Hello {}! id:{}", info.0, info.1)
@@ -6,10 +8,17 @@ async fn index(info: web::Path<(String, u32)>) -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let mut listenfd = ListenFd::from_env();
+    let listener = if let Some(listener) = listenfd.take_tcp_listener(0)? {
+        listener
+    } else {
+        TcpListener::bind("127.0.0.1:8080")?
+    };
+
     HttpServer::new(|| App::new().service(
         web::resource("/{name}/{id}/index.html").to(index))
     )
-        .bind("127.0.0.1:8080")?
+        .listen(listener)?
         .run()
         .await
 }
